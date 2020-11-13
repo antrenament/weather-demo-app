11a1d563fc2b82a0eba58ee4bb110be6c36d29db
antrenament <intentart@gmail.com>
2020-11-13 14:57:55 +0000

    Add fetch request, to weather api, displaying cards with data

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Card from "@material-ui/core/Card";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import CardActions from "@material-ui/core/CardActions";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import Grid from "@material-ui/core/Grid";
 
-const WeatherCard = ({location}) => {
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+const WeatherCard = ({ location }) => {
+  const classes = useStyles()
+  const [weatherData, setWeatherData] = useState([])
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY
+  const API_URL = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}`
+
+  useEffect(() => {
+    fetchData()
+  }, [location])
+
+  const fetchData = async () => {
+    const response = await fetch(API_URL)
+    const data = await response.json()
+    setWeatherData(data)
+  }
+
+
   return (
-    <div>{location}</div>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardContent>
+          <Grid container>
+            <Grid item xs={10}>
+              <Typography gutterBottom variant="h5" component="h2">
+                {weatherData?.location?.name}
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              <Avatar src={weatherData?.current?.condition.icon} sizes={"40px"}/>
+            </Grid>
+          </Grid>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Local Time: {weatherData?.location?.localtime}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Temperature: {weatherData?.current?.temp_c + 'C'}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   )
 }
 
