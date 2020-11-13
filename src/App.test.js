f8a633fbd2470f97cc2f19a3295399a1520341b4
antrenament <intentart@gmail.com>
2020-11-13 12:35:22 +0000

    Remove files which would not be used for this app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
+import React from 'react'
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('renders Welcome text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const welcomeMessage = screen.getByText('Welcome')
+  expect(welcomeMessage).toBeInTheDocument()
 });
