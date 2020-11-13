b073e7bc60a98d82cbc4b8ac9535253bcd1be96d
antrenament <intentart@gmail.com>
2020-11-13 13:28:31 +0000

    Adding icons from material ui, adding a textField component to render the input field

--- src/components/InputLocation/InputLocation.js
+++ src/components/InputLocation/InputLocation.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import TextField from '@material-ui/core/TextField'
+import {InputAdornment} from "@material-ui/core";
+import AddIcon from '@material-ui/icons/Add';
+import IconButton from "@material-ui/core/IconButton";
 
 const InputLocation = () => {
-  return <div>Input your favorite locations</div>
+  return (
+    <TextField
+      placeholder={"Type your favorite location. Use city name or postal code"}
+      multiline
+      variant={"outlined"}
+      rowsMax={2}
+      fullWidth
+      InputLabelProps={{ color: 'salmon', focused: true}}
+      InputProps={{
+        endAdornment: <InputAdornment position={"end"}>
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+        </InputAdornment>
+      }}
+    />
+  )
 }
 
 export default InputLocation
